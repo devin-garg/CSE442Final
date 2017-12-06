fd47a613c8ebd11a4ad00cdbfe1e29b6734c1e0f
devin-garg <devin.garg@s-sm.org>
2017-12-06 00:16:11 -0800

    Add files via upload

---
+++ StackTrace.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+class StackTrace extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  handleChange(event) {
+    this.props.updateProps({
+      value: +event.target.value
+    });
+  }
+
+  render() {
+    var { value, min, max, step } = this.props;
+  console.log(this.props)
+    return (
+      <input type="range" onChange={this.handleChange.bind(this)} value={value} min={min} max={max} step={step} />
+    );
+  }
+
+}
+
+StackTrace.defaultProps = {
+  value: 0,
+  min: 0,
+  max: 1,
+  step: 1
+};
+
+export default StackTrace;
