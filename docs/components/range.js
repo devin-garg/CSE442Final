c62b180d240fde55afd7b7c30d139fb6dc20cb88
ryanfok <ryanfok@cs.washington.edu>
2017-12-06 23:28:02 -0800

    final files

--- docs/components/range.js
+++ docs/components/range.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
 
-class StackTrace extends React.Component {
+class Range extends React.PureComponent {
   constructor(props) {
     super(props);
   }
 
   handleChange(event) {
     this.props.updateProps({
-      value: +event.target.value
+      value: +event.target.value,
+      changingStack: event.target.value,
+      maxStack: event.target.value
     });
   }
 
   render() {
     var { value, min, max, step } = this.props;
-  console.log(this.props)
+
     return (
       <input type="range" onChange={this.handleChange.bind(this)} value={value} min={min} max={max} step={step} className="slider"/>
     );
@@ -22,11 +24,11 @@ class StackTrace extends React.Component {
 
 }
 
-StackTrace.defaultProps = {
+Range.defaultProps = {
   value: 0,
   min: 0,
   max: 1,
   step: 1
 };
 
-module.exports = StackTrace;
+module.exports = Range;
